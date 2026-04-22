4a40cf721878983fd2560072a5f3dbd5d053f01d
Marco Souza <marco.souza@mongodb.com>
2026-04-22 17:01:12 -0300

    fea: set maestro

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,14 @@ Copyright © 2025 Marco Souza <marco@tremtec.com>
 package cmd
 
 import (
+	"fmt"
+	"os"
+
+	"marco-souza/djc/internal/config"
+	"marco-souza/djc/internal/library"
+	"marco-souza/djc/internal/tui"
+
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -12,6 +20,26 @@ var rootCmd = &cobra.Command{
 	Use:   "djc",
 	Short: "A CLI toolbox for DJing",
 	Long:  `djc is a CLI tool for DJs to download and manage music from YouTube.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg, err := config.Load()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
+			os.Exit(1)
+		}
+
+		repo, err := library.Open(cfg.DatabasePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error opening database: %v\n", err)
+			os.Exit(1)
+		}
+		defer repo.Close()
+
+		program := tea.NewProgram(tui.NewModel(repo, cfg), tea.WithAltScreen())
+		if _, err := program.Run(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error running TUI: %v\n", err)
+			os.Exit(1)
+		}
+	},
 }
 
 func Execute() {
